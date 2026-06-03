08f27a78d8f0caf624694c068fff1b4964dd873a
Soft Install <softinstall@ailogic.com>
2026-06-03 13:27:33 +0530

    Test case 1

--- Signal.py
+++ Signal.py
@@ -1,7 +1,21 @@
 import math
-n=6
-l=[2,1,5,1,3,2]
-k=2
+#  expalnation 
+# n=6
+# l=[2,1,5,1,3,2]
+# k=2
+# f=0
+# m=[]
+
+# for i in range(1,n,k):
+#     f=f+i
+#     m.append(f)
+
+# s=max(m)
+# print(s)
+
+n=5
+l=[4,2,7,1,9]
+k=1
 f=0
 m=[]
 
